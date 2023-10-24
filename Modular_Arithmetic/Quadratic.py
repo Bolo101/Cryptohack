bd3e0f62b090f687060de4514f7048f67fa8085d
bololo7 <101951542+bololo7@users.noreply.github.com>
2023-10-24 22:16:08 +0200

    Correct version without comments

--- Modular_Arithmetic/Quadratic.py
+++ Modular_Arithmetic/Quadratic.py
@@ -1,25 +1,32 @@
 """
-Basic brute forcing quadratic residues implement
+Basic quadratic residue implement
 x is called a quadratric residues if it solves a**2 = x [p]
 """
 import sys
 from math import sqrt
 def quadratic(square,p):
     for i in range(p):
-        if (square - i) % p == False:
+        if (square - i ) % p == 0:
             quadri = i
+            print(f"Quadratic residue = {quadri}")
             break
-    a1 = sqrt(quadri)
-    a2 = - sqrt(quadri)
+    a1 = sqrt(square) 
+    #a2 = sqrt(square) * (-1)
+    #print(f"{a1} ; {a2}")
+    if a1 == int(a1):
+        return[int(a1),quadri]
+    else:
+        print("Floating solution")
+        return [False,False]
+        
     
 
 def main():
     text = int(input("Enter a signed integer:"))
     modulo = int(input("Enter the modulo:"))
-    result = quadratic(text,modulo)
-    if result:
-        print("Done")
-        print(result)
+    root,quadri = quadratic(text,modulo)
+    if root:
+        print(f"{root} is the square root of {quadri} mod[{modulo}]")
     else:
         print("No result")
         sys.exit()
