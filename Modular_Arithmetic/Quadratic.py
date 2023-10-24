a1ff667ea76aaa8697e612df0497754ae5492d81
bololo7 <101951542+bololo7@users.noreply.github.com>
2023-10-24 22:31:36 +0200

    Corrected, misunderstood the concept at first

--- Modular_Arithmetic/Quadratic.py
+++ Modular_Arithmetic/Quadratic.py
@@ -1,35 +1,18 @@
-"""
-Basic quadratic residue implement
-x is called a quadratric residues if it solves a**2 = x [p]
-"""
 import sys
-from math import sqrt
-def quadratic(square,p):
+
+def quadratic(square, p):
     for i in range(p):
-        if (square - i ) % p == 0:
-            quadri = i
-            print(f"Quadratic residue = {quadri}")
-            break
-    a1 = sqrt(square) 
-    #a2 = sqrt(square) * (-1)
-    #print(f"{a1} ; {a2}")
-    if a1 == int(a1):
-        return[int(a1),quadri]
-    else:
-        print("Floating solution")
-        return [False,False]
-        
-    
+        if (i * i) % p == square:
+            return i
 
 def main():
-    text = int(input("Enter a signed integer:"))
-    modulo = int(input("Enter the modulo:"))
-    root,quadri = quadratic(text,modulo)
-    if root:
-        print(f"{root} is the square root of {quadri} mod[{modulo}]")
+    text = int(input("Enter a signed integer: "))
+    modulo = int(input("Enter the modulo: "))
+    quadri = quadratic(text, modulo)
+    if quadri is not None:
+        print(f"{text} is a quadratic residue mod {modulo}, with a = {quadri}.")
     else:
-        print("No result")
-        sys.exit()
+        print(f"{text} is not a quadratic residue mod {modulo}.")
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     main()
