71cfcb6813ef199acac05e1d46f376b3e21d7153
bololo7 <101951542+bololo7@users.noreply.github.com>
2023-12-19 20:51:29 +0100

    Back to work

--- Modular_Arithmetic/Quadratic.py
+++ Modular_Arithmetic/Quadratic.py
@@ -1,11 +1,18 @@
 import sys
-
+"""
+quadratic is a function that enables to determine if a number "square" is a quadratic residue by matching 
+this number with its possible square root. If we find a square root for "square" mod p then "square is a 
+quadratic residue of p
+"""
 def quadratic(square, p):
     for i in range(p):
         if (i * i) % p == square:
             return i
     return None
-
+"""
+We say that an integer x is a Quadratic Residue if there exists an a such that a² = x mod p
+a being the sqare root
+"""
 def main():
     text = int(input("Enter a signed integer: "))
     modulo = int(input("Enter the modulo: "))
